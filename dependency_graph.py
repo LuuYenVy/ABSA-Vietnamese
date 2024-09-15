48452e22cba44e4927cda8adf681b40e0654bc49
LuuYenVy <81798556+LuuYenVy@users.noreply.github.com>
2024-09-16 00:23:03 +0700

    Update dependency_graph.py

--- dependency_graph.py
+++ dependency_graph.py
@@ -1,65 +1,46 @@
-# -*- coding: utf-8 -*-
-
+from vncorenlp import VnCoreNLP
 import numpy as np
-import spacy
 import pickle
 import argparse
 
+# Đường dẫn tới mô hình VnCoreNLP
+model_path = "D:\Thuc tap\ABSA-PyTorch\models\VnCoreNLP-1.1.1"
 
-from spacy.tokens import Doc
-
-class WhitespaceTokenizer(object):
-    def __init__(self, vocab):
-        self.vocab = vocab
-
-    def __call__(self, text):
-        words = text.split()
-        # All tokens 'own' a subsequent space character in this tokenizer
-        spaces = [True] * len(words)
-        return Doc(self.vocab, words=words, spaces=spaces)
-
-nlp = spacy.load('en_core_web_sm')
-nlp.tokenizer = WhitespaceTokenizer(nlp.vocab)
+# Khởi tạo VnCoreNLP
+vnlp = VnCoreNLP(model_path)
 
 def dependency_adj_matrix(text):
-    # https://spacy.io/docs/usage/processing-text
-    tokens = nlp(text)
-    words = text.split()
+    # Phân tích văn bản
+    parsed = vnlp.parse(text)
+    words = [word for sentence in parsed for word in sentence]
+    word_indices = {word[0]: i for i, word in enumerate(words)}
+    
     matrix = np.zeros((len(words), len(words))).astype('float32')
-    assert len(words) == len(list(tokens))
-
-    for token in tokens:
-        matrix[token.i][token.i] = 1
-        for child in token.children:
-            matrix[token.i][child.i] = 1
-            matrix[child.i][token.i] = 1
-
+    
+    for sentence in parsed:
+        for word in sentence:
+            head = word[1]
+            if head != 0:  # 0 có nghĩa là không có head
+                matrix[word_indices[head]][word_indices[word[0]]] = 1
+                matrix[word_indices[word[0]]][word_indices[head]] = 1
+    
     return matrix
 
 def process(filename):
-    fin = open(filename, 'r', encoding='utf-8', newline='\n', errors='ignore')
-    lines = fin.readlines()
-    fin.close()
+    with open(filename, 'r', encoding='utf-8', newline='\n', errors='ignore') as fin:
+        lines = fin.readlines()
+    
     idx2graph = {}
-    fout = open(filename+'.graph', 'wb')
-    for i in range(0, len(lines), 3):
-        text_left, _, text_right = [s.strip() for s in lines[i].partition("$T$")]
-        aspect = lines[i + 1].strip()
-        adj_matrix = dependency_adj_matrix(text_left+' '+aspect+' '+text_right)
-        idx2graph[i] = adj_matrix
-    pickle.dump(idx2graph, fout)        
-    fout.close() 
+    with open(filename + '.graph', 'wb') as fout:
+        for i in range(0, len(lines), 3):
+            text_left, _, text_right = [s.strip() for s in lines[i].partition("$T$")]
+            aspect = lines[i + 1].strip()
+            adj_matrix = dependency_adj_matrix(text_left + ' ' + aspect + ' ' + text_right)
+            idx2graph[i] = adj_matrix
+        pickle.dump(idx2graph, fout)        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', default=None, type=str, help='path to dataset')
     opt = parser.parse_args()
     process(opt.dataset)
-
-    # process('./datasets/acl-14-short-data/train.raw')
-    # process('./datasets/acl-14-short-data/test.raw')
-    # process('./datasets/semeval14/Restaurants_Train.xml.seg')
-    # process('./datasets/semeval14/Restaurants_Test_Gold.xml.seg')
-    # process('./datasets/semeval14/Laptops_Train.xml.seg')
-    # process('./datasets/semeval14/Laptops_Test_Gold.xml.seg')
-
